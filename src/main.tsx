5e16e287fbf9b54eba76fc4f84d562c63ae5ca8b
Henrique Kramer <henriquemkramer@gmail.com>
2025-02-04 23:55:19 -0300

    feat: add routes structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
+import { App } from "./App";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Dashboard } from "./pages/app/dashboard";
+import { SignIn } from "./pages/auth/sign-in";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
 );
