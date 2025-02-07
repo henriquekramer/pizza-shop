02a0e4cabbb1b33ad6092f41ab19b9a891407c99
Henrique Kramer <henriquemkramer@gmail.com>
2025-02-07 00:10:05 -0300

    feat: add theme-toggle and dropdown-menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Router } from "./routes";
 import { Toaster } from "sonner";
 import "./index.css";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors />
-      <Router />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <Router />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
