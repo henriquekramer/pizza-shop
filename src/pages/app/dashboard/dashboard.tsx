19e1e615182a0cad146457900e9712708d1c3079
Henrique Kramer <henriquemkramer@gmail.com>
2025-02-09 11:43:17 -0300

    feat: add renevue chart

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -4,6 +4,7 @@ import { MonthRevenueCard } from "./month-revenue-card";
 import { MonthOrdersAmountCard } from "./month-orders-amount-card";
 import { DaysOrdersAmountCard } from "./days-orders-amount-card";
 import { MonthCanceledOrdersAmount } from "./month-canceled-orders-amount";
+import { RevenueChart } from "./revenue-chart";
 
 export function Dashboard() {
   return (
@@ -16,6 +17,10 @@ export function Dashboard() {
           <MonthOrdersAmountCard />
           <DaysOrdersAmountCard />
           <MonthCanceledOrdersAmount />
+        </div>
+
+        <div className="grid grid-cols-9 gap-4">
+          <RevenueChart />
         </div>
       </div>
     </Fragment>
