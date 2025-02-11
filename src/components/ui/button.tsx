36cd2f21abb363d7910be322e953256e19fd46df
Henrique Kramer <henriquemkramer@gmail.com>
2025-02-10 22:58:53 -0300

    feat: create store profile dialog

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,6 +17,8 @@ const buttonVariants = cva(
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+        success:
+          "bg-emerald-500 text-white shadow-sm hover:bg-emerald-500/90 dark:hover:bg-emerald-600/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
