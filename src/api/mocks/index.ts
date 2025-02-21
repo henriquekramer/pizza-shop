caf2ceefeccd1aaabae0c0b1674fcff98703a93f
Henrique Kramer <henriquemkramer@gmail.com>
2025-02-21 00:15:34 -0300

    feat: orders mocks

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,15 +2,17 @@ import { env } from "@/env";
 import { setupWorker } from "msw/browser";
 import { signInMock } from "./sign-in-mock";
 import { registerRestaurantMock } from "./register-restaurant-mock";
-import { getDayOrdersAmountMock } from "./get-day-orders-amount";
-import { getMonthCanceledOrdersAmountMock } from "./get-month-canceled-orders";
-import { getMonthRevenuetMock } from "./get-month-revenue";
-import { getMonthOrdersAmountMock } from "./get-month-orders-amount";
+import { getDayOrdersAmountMock } from "./get-day-orders-amount-mock";
+import { getMonthCanceledOrdersAmountMock } from "./get-month-canceled-orders-mock";
+import { getMonthRevenuetMock } from "./get-month-revenue-mock";
+import { getMonthOrdersAmountMock } from "./get-month-orders-amount-mock";
 import { getDailyRevenueInPeriodMock } from "./get-daily-revenue-in-period-mock";
 import { getPopularProductsMock } from "./get-popular-products-mock";
-import { getManagedRestaurantMock } from "./get-managed-restaurant-mcok";
+import { getManagedRestaurantMock } from "./get-managed-restaurant-mock";
 import { getProfileMock } from "./get-profile-mock";
 import { updateProfileMock } from "./update-profile-mock";
+import { getOrderDetailsMock } from "./get-order-details-mock";
+import { getOrdersMock } from "./get-orders-mock";
 
 export const worker = setupWorker(
   signInMock,
@@ -24,6 +26,8 @@ export const worker = setupWorker(
   updateProfileMock,
   getProfileMock,
   getManagedRestaurantMock,
+  getOrdersMock,
+  getOrderDetailsMock,
 );
 
 export async function enableMSW() {
