375a9ee04be80ed502f691673af0b6a0daed7537
Henrique Kramer <henriquemkramer@gmail.com>
2025-02-22 18:03:09 -0300

    test: add async assertions

--- test/store-profile.e2e-spec.ts
+++ test/store-profile.e2e-spec.ts
@@ -17,11 +17,11 @@ test("update profile successfully", async ({ page }) => {
 
   const toast = page.getByText("Perfil atualizado com sucesso");
 
-  expect(toast).toBeVisible();
+  await expect(toast).toBeVisible();
 
   await page.getByRole("button", { name: "Cancelar" }).click();
 
-  await page.waitForTimeout(250);
-
-  expect(page.getByRole("button", { name: "Rocket Pizza" })).toBeVisible();
+  await expect(
+    page.getByRole("button", { name: "Rocket Pizza" }),
+  ).toBeVisible();
 });
